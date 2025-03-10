9458c1952d1363aec9187d57524f0110ee08b0ee
ChrisYzc <129034802+ChrisYzc@users.noreply.github.com>
2025-03-10 22:08:03 +0800

    Update main.py

--- main.py
+++ main.py
@@ -144,7 +144,7 @@ if __name__=='__main__':
         temp_list = get_channel_http(channel_url)
         url_list.extend(temp_list)
     logger.info('开始筛选---')
-    thread_max_num = threading.Semaphore(32)  # 32线程
+    thread_max_num = threading.Semaphore(16)  # 16线程#可以更改为32、8、4
     bar = tqdm(total=len(url_list), desc='订阅筛选：')
     thread_list = []
     for url in url_list:
